4e0ead1082711a514c8bf1d15b6e43586aa85f21
Prabal Gupta <prabal.gupta.1997@ieee.org>
2016-05-16 21:49:29 -0400

    Add files via upload

---
+++ lcd_text_display.ino
@@ -0,0 +1,127 @@
+
+#include<LiquidCrystal.h>
+
+//OBJECT CREATION
+LiquidCrystal lcd(12,11,5,4,3,2);
+const int switchPin=6;
+int j=0;
+signed int i=0;
+byte happy[8]     = { B00000, B01010, B00000, B00000, B10001, B01110, B00000 };
+byte sad[8]       = { B00000, B01010, B00000, B00000, B01110, B10001, B00000 };
+byte tick[8]      = { B00000, B00000, B00001, B10010, B10100, B11000, B10000 };
+byte cross[8]     = { B10001, B10001, B01010, B00100, B01010, B10001, B10001 };
+byte template1[8]
+={
+B00100,
+B01100,
+B01100,
+B01100,
+B01110,
+B11111,
+B11111
+};
+//byte template2[8] = { B00000, B00000, B00001, B10010, B10100, B11000, B10000 };
+//byte template3[8] = { B10001, B10001, B01010, B00100, B01010, B10001, B10001 };
+
+//CONFIGURATION
+String input_data=""; //USE ESCAPE CHARACTERS TO ACCESS LETTERS I.E \1 WILL PRINT 'HAPPY' ICON (WARNING: \0 DOES NOT PRINT ANYTHING!)
+const int PRINT_DELAY=15;
+
+void setup() {
+  pinMode(switchPin, INPUT); 
+  lcd.createChar(1, happy);
+  lcd.createChar(2, sad);
+  lcd.createChar(3, cross);
+  lcd.createChar(4, tick);
+  lcd.createChar(5, template1);
+//  lcd.createChar(6, template1);
+//  lcd.createChar(7, template1);
+  lcd.clear();
+  lcd.begin(16,2);
+  Serial.begin(9600);
+}
+
+//DATA-PRINTING
+void print_lcd(String& input) {
+static int counter=0;
+  lcd.setCursor(0,0);
+  input.trim();
+  //EXCEPTION HANDLING FOR WHEN A BLANK INPUT IS RECEIVED
+  if (input=="") {
+    input=" ";
+  }
+  for (j=0; j<=(input.length()/16); ++j) {
+    
+    //CHOOSING APPROPRIATE ROW TO PRINT ON
+    if ((j%2)==0) {
+      lcd.clear();
+      lcd.setCursor(0,0);
+    }
+    else {
+      lcd.setCursor(0,1);
+    }
+    //FIRST PART OF THE 'FOR LOOP'
+    i=0;
+    
+     if(input[(j*16)+i]==' ') {
+      ++i;
+      }
+    
+    //SECOND AND THIRD PART OF THE 'FOR LOOP'
+    for (; i<= ((j==(input.length()/16))?((input.length()%16)-1):(15)) ; ++i) {
+      //CHECKING FOR SPECIAL SYMBOLS
+      if (input[(j*16)+i]=='|') {
+        i+=1;
+        if (input[(j*16)+i]=='1') {
+          lcd.print('\1');
+        }
+        else if (input[(j*16)+i]=='2'){
+          lcd.print('\2');
+        }
+        else if (input[(j*16)+i]=='3') {
+          lcd.print('\3');          
+        }
+        else if (input[(j*16)+i]=='4') {
+          lcd.print('\4');
+        }
+        else if (input[(j*16)+i]=='5') {
+          lcd.print('\5');
+        }
+  //      else if (input[(j*16)+i]=='6') {
+  //        lcd.print('\6');
+  //      }
+  //      else if (input[(j*16)+i]=='7') {
+  //        lcd.print('\7');
+  //      }
+      }
+      else {
+      //PRINTING NORMAL TEXT
+      lcd.print(input[(j*16)+i]);
+    }
+  delay(PRINT_DELAY);
+  }
+ }
+}
+
+void loop() {
+  
+  static int repeat=0;
+  
+  //RECEVING INPUT FROM THE SERIAL
+  while(Serial.available()) {
+    input_data+=(char)Serial.read();
+  }
+  
+  //PRINTING DATA
+  if(!Serial.available()) {
+    if(input_data!="") {
+      Serial.print(input_data);
+      Serial.print("\n");
+      print_lcd(input_data);
+    }
+  }
+  input_data="";
+  
+  delay(3000);
+}
+
---
+++ loading_bar.ino
@@ -0,0 +1,62 @@
+#include<LiquidCrystal.h>
+
+//OBJECT CREATION
+LiquidCrystal lcd(12,11,5,4,3,2);
+const int switchPin=6;
+
+//BYTES
+byte first[8]   = { B10000, B10000, B10000, B10000, B10000, B10000, B10000, 16};
+byte second[8]  = { B11000, B11000, B11000, B11000, B11000, B11000, B11000, 24};
+byte third[8]   = { B11100, B11100, B11100, B11100, B11100, B11100, B11100, 28};
+byte fourth[8]  = { B11110, B11110, B11110, B11110, B11110, B11110, B11110, 30};
+byte full[8]    = { B11111, B11111, B11111, B11111, B11111, B11111, B11111, 31};
+
+void setup() {
+  //CONFIGURATION
+  pinMode(switchPin, INPUT);
+  lcd.clear();
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  //LETTERS SETUP
+  lcd.createChar(1, first);
+  lcd.createChar(2, second);
+  lcd.createChar(3, third);
+  lcd.createChar(4, fourth);
+  lcd.createChar(5, full);
+}
+
+void show_loading(int time) {
+  time*=1000;
+  time/=80;
+  
+  //PRINTING LETTERS
+  lcd.setCursor(0,0);
+  lcd.print("Progress: ");
+    
+  //PRINTING LOADING BAR AND THE PERCENTAGE
+  float value=0;
+  for (int j=0; j<=15; ++j) {
+    lcd.setCursor(j,1);
+    for (int i=1; i<=5; ++i) {
+      lcd.setCursor(j,1);
+      lcd.write(byte(i));
+      delay(time);
+      
+      lcd.setCursor(10,0);
+      value+=(((float)5)/4);
+      lcd.print(value);
+
+      if (value>=99.99) {
+        lcd.setCursor(15,0);
+      }
+      lcd.print("%");
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  show_loading(5); //NOTE: Input is in seconds
+  delay(5000);
+  lcd.clear();
+}
